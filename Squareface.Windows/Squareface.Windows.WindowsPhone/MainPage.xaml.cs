b69855a976e0e541ab8435406fcfbbdbd23a74f1
oliverDOTnet <amdcorei7>
2015-05-09 15:10:01 +0200

    FilePicker & Continuation implemented

--- Squareface.Windows/Squareface.Windows.WindowsPhone/MainPage.xaml.cs
+++ Squareface.Windows/Squareface.Windows.WindowsPhone/MainPage.xaml.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
@@ -22,6 +28,9 @@ namespace Squareface.Windows
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private BitmapImage SourceImage;
+        private CoreApplicationView view;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -37,12 +46,62 @@ namespace Squareface.Windows
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             // TODO: Seite vorbereiten, um sie hier anzuzeigen.
+            SourceImage = new BitmapImage();
 
             // TODO: Wenn Ihre Anwendung mehrere Seiten enthält, stellen Sie sicher, dass
             // die Hardware-Zurück-Taste behandelt wird, indem Sie das
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed-Ereignis registrieren.
             // Wenn Sie den NavigationHelper verwenden, der bei einigen Vorlagen zur Verfügung steht,
             // wird dieses Ereignis für Sie behandelt.
+        }
+
+        private async void LoadImage()
+        {
+            view = CoreApplication.GetCurrentView();
+
+            FileOpenPicker openPicker = new FileOpenPicker();
+
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+
+            // Dateitypen filtern
+            openPicker.FileTypeFilter.Clear();
+            openPicker.FileTypeFilter.Add(".bmp");
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            openPicker.FileTypeFilter.Add(".jpg");
+
+            // Filepicker starten
+            openPicker.PickSingleFileAndContinue();
+            view.Activated += viewActivated;
+        }
+
+        private async void viewActivated(CoreApplicationView sender, IActivatedEventArgs args1)
+        {
+            FileOpenPickerContinuationEventArgs args = args1 as FileOpenPickerContinuationEventArgs;
+
+            if (args != null)
+            {
+                if (args.Files.Count == 0) return;
+
+                view.Activated -= viewActivated;
+                var file = args.Files[0];
+
+                // Nutzer hat den Dialog abgebrochen
+                if (file != null)
+                {
+                    IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
+
+                    SourceImage.SetSource(fileStream);
+                    testImage.Source = SourceImage;
+                    this.DataContext = file;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadImage();
         }
     }
 }
